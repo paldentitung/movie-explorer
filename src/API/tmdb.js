23d580ee09d29b85568905497e0c511c502e59d3
Palden D. Titung <paldendorjetitung@gmail.com>
2025-10-31 01:20:50 +0545

    added new api key

--- src/API/tmdb.js
+++ src/API/tmdb.js
@@ -3,6 +3,9 @@ const BASE_URL = "https://api.themoviedb.org/3";
 
 // Trending movies
 export const getTrendingMovies = async () => {
+  if (!API_KEY) {
+    throw new Error("API key is missing. Check your .env file");
+  }
   const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
   const data = await res.json();
   return data.results;
